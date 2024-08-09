4b00c37d6213de60feec0b2a16285b0754102be9
danial <mdanialfarokhi@gmail.com>
2024-08-09 21:56:13 +0330

    end footer + js coflict btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,54 @@
-  $('.custom-slider').slick({
+  $('.custom-slider1').slick({
 
     rtl: true,
     autoplay: true,
     arrows: true,
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
+    dots:true,
+    focusOnSelect:true,
+    accessibility: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    responsive: [
+      {  
+          breakpoint: 1000,
+          settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }},
+      {
+        
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.custom-slider2').slick({
+
+    rtl: true,
+    autoplay: true,
+    arrows: true,
+    prevArrow: $('.prev1'),
+    nextArrow: $('.next1'),
     dots:true,
     focusOnSelect:true,
     accessibility: true,
